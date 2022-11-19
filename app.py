668d1770f9a5b683344857d7f9969454739ad09d
saursing <33507188+saursing@users.noreply.github.com>
2022-11-19 10:01:50 -0800

    updated app.py with /hello/<name> logic

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 #imports
 import os
+import re
 from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # initializing Flask app
 app = Flask(__name__)
@@ -31,6 +33,21 @@ class Users(db.Model):
 def hello_world():
     name = os.environ.get("NAME", "World")
     return "Hello {}!".format(name)
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X")
+
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello " + clean_name + "! It's " + formatted_now
+    return content
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=8080)
