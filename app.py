a806d20292a9c8bff9d2acdf5c4ef13e1c3f4e16
saursing <33507188+saursing@users.noreply.github.com>
2022-11-19 13:01:18 -0800

    removed sql logic

--- app.py
+++ app.py
@@ -8,26 +8,6 @@ from datetime import datetime
 # initializing Flask app
 app = Flask(__name__)
 
-# Google Cloud SQL (change this accordingly)
-PASSWORD ="Test#1234"
-PUBLIC_IP_ADDRESS ="35.225.130.143"
-DBNAME ="Test"
-PROJECT_ID ="docker-flask-358409"
-INSTANCE_NAME ="gcp-flask-sql1"
-Link="mysql+mysqldb://root:Test#1234@35.225.130.143/Test?unix_socket=/cloudsql/docker-flask-358409:gcp-flask-sql1"
-
-# configuration
-app.config["SECRET_KEY"] = "yoursecretkey"
-app.config["SQLALCHEMY_DATABASE_URI"]= Link
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]= True
-
-db = SQLAlchemy(app)
-
-# User ORM for SQLAlchemy
-class Users(db.Model):
-  id = db.Column(db.Integer, primary_key = True, nullable = False)
-  name = db.Column(db.String(50), nullable = False)
-  email = db.Column(db.String(50), nullable = False, unique = True)
 
 @app.route("/")
 def hello_world():
