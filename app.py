0b1643d2e0c02b9e10710a369baa5dc077a7f78f
saursing <33507188+saursing@users.noreply.github.com>
2022-11-20 15:28:41 -0800

    updates with sql view and sql insert logic

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #imports
 import os
 import re
+import sqlalchemy
 from flask import Flask, request, make_response
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
@@ -9,6 +10,99 @@ from datetime import datetime
 app = Flask(__name__)
 
 
+connection_name = "rb-munish-playground:us-central1:test-sql"
+table_name = "entries"
+table_field_1 = "guestName"
+table_field_value_1 = "saurabh"
+table_field_2 = "content"
+table_field_value_2 = "sample test"
+db_name = "testing"
+db_user = "root"
+db_password = "1234"
+sql_host="35.192.177.19"
+sql_port="3306"
+
+driver_name = 'mysql+pymysql'
+query_string = dict({"unix_socket": "/cloudsql/{}".format(connection_name)})
+
+
+@app.route('/insert/<request>')
+def add(request):
+    # getting name and email
+    match_object = re.match("name=(.*)&content=(.*)", request)
+    name = match_object.group(1)
+    content = match_object.group(2)
+    stmt = sqlalchemy.text('INSERT INTO entries (guestName, content) values ("{}","{}")'.format(name, content))
+
+    db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername=driver_name,
+        username=db_user,
+        password=db_password,
+        host=sql_host,
+        port=sql_port,
+        database=db_name,
+        query=query_string,
+      ),
+      pool_size=5,
+      max_overflow=2,
+      pool_timeout=30,
+      pool_recycle=1800
+    )
+    
+    try:
+        with db.connect() as conn:
+            conn.execute(stmt)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
+    
+    responseObject = {
+        'status' : 'success',
+        'message': 'Successfully registered.'
+    }
+ 
+    return make_response(responseObject, 200)
+
+ 
+ 
+@app.route("/viewsql/")
+def connect_sql():
+    stmt = sqlalchemy.text('SELECT * FROM entries')
+
+    db = sqlalchemy.create_engine(
+      sqlalchemy.engine.url.URL(
+        drivername=driver_name,
+        username=db_user,
+        password=db_password,
+        host=sql_host,
+        port=sql_port,
+        database=db_name,
+        query=query_string,
+      ),
+      pool_size=5,
+      max_overflow=2,
+      pool_timeout=30,
+      pool_recycle=1800
+    )
+
+    try:
+        with db.connect() as conn:
+            response = conn.execute(stmt)
+    except Exception as e:
+        return 'Error: {}'.format(str(e))
+    
+    output = list()
+    for user in response:
+        output.append({
+            "name" : user.guestName,
+            "content": user.content
+        })
+        
+    return make_response({
+        'status' : 'success',
+        'message': output
+    }, 200)
+    
 @app.route("/")
 def hello_world():
     name = os.environ.get("NAME", "World")
